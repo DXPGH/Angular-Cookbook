7e1dd44fcdb5559ff72683aa574384408c0460f0
DXPGH <dxpbusiness@gmail.com>
2022-10-25 09:42:25 -0500

    finished creating a singleton service using providedIn

--- chapter02/start_here/ad-attribute-directive/src/app/directives/highlight.directive.ts
+++ chapter02/start_here/ad-attribute-directive/src/app/directives/highlight.directive.ts
@@ -1,27 +1,35 @@
-import { Directive, Input, SimpleChanges, OnChanges, ElementRef } from '@angular/core';
+import {
+  Directive,
+  Input,
+  SimpleChanges,
+  OnChanges,
+  ElementRef,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appHighlight]',
 })
 export class HighlightDirective implements OnChanges {
-@Input() highlightText = '';
-@Input() highlightColor = 'yellow';
-originalHTML = '';
+  @Input() highlightText = '';
+  @Input() highlightColor = 'yellow';
+  originalHTML = '';
+  constructor(private el: ElementRef) {}
 
-  constructor(private el: ElementRef) { }
-
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges) {
     if (changes.highlightText.firstChange) {
       this.originalHTML = this.el.nativeElement.innerHTML;
       return;
     }
-    const {currentValue} = changes.highlightText;
+    const { currentValue } = changes.highlightText;
     if (currentValue) {
-      const regExp = new RegExp(`(${currentValue})`,'gi')
-      this.el.nativeElement.innerHTML = this.originalHTML.replace(regExp, `<span style="background-color:${this.highlightColor}">\$1</span>`)
-      } else {
-        this.el.nativeElement.innerHTML = this.originalHTML;
-      }
+      const regExp = new RegExp(`(${currentValue})`, 'gi');
+      this.el.nativeElement.innerHTML = this.originalHTML.replace(
+        regExp,
+        `<span style="background-color: ${this.highlightColor}">\$1</span>`
+      );
+    } else {
+      this.el.nativeElement.innerHTML = this.originalHTML;
+    }
+    console.log(currentValue);
   }
-
 }
